6be3828fa2314259fb59ba544fccb4811e974fc5
xav <xavier@loyaltylion.com>
2017-12-28 19:32:49 +0000

    add password reset migration etc

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
 
   resources :users
-  resources :account_activations, only: [:edit]
+  resources :account_activations, only: %i[edit]
+  resources :password_resets,     only: %i[new create edit update]
 end
