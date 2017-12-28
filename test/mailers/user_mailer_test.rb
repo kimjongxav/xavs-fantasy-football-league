50a4d00f27885f17ddbee8aa0d485c4411c22fed
xav <xavier@loyaltylion.com>
2017-12-28 20:24:47 +0000

    password reset working

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -12,4 +12,15 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token,   mail.body.encoded
     assert_match CGI.escape(user.email),  mail.body.encoded
   end
+
+  test 'password_reset' do
+    user = users(:frodo)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+    assert_equal 'Reset password', mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ['noreply@example.com'], mail.from
+    assert_match user.reset_token,   mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
+  end
 end
