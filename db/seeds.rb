f28037202fa9ecf042635bcb304955532d845248
xav <xavier@loyaltylion.com>
2017-12-29 18:44:37 +0000

    add micrposts to user show

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.create!(
   password_confirmation: 'foobar',
   admin: true,
   activated: true,
-  activated_at: DateTime.now
+  activated_at: Time.zone.now
 )
 
 99.times do |n|
@@ -28,6 +28,12 @@ User.create!(
     password: password,
     password_confirmation: password,
     activated: true,
-    activated_at: DateTime.now
+    activated_at: Time.zone.now
   )
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
