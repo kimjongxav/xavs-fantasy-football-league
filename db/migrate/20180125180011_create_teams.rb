f0d13ec28a9c1e955dcee716d1d3e64becf2dda3
xav <xavier@loyaltylion.com>
2018-01-25 18:11:32 +0000

    make team properties attribute a string

--- db/migrate/20180125180011_create_teams.rb
+++ db/migrate/20180125180011_create_teams.rb
@@ -3,7 +3,7 @@ class CreateTeams < ActiveRecord::Migration[5.1]
     create_table :teams do |t|
       t.string :name
       t.integer :transfers_remaining
-      t.json :properties
+      t.string :properties # this will be a string in the style of json
       (1..38).to_a.map { |i| t.integer "points_in_gameweek_#{i}" }
 
       t.timestamps
