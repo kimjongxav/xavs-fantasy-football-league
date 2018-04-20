affac96ed0d5143e2e08fb4cefcafa9101cae23d
xav <xavier@loyaltylion.com>
2018-04-20 21:32:30 +0100

    players index working

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,9 @@
 class PlayersController < ApplicationController
+  before_action :logged_in_user, only: [:index]
+
+  def index
+    @players = Player.paginate(page: params[:page])
+  end
 
   def show
     @player = Player.find(params[:id])
