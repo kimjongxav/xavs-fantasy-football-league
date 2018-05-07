c23e3e27452ea0453bcaddc165b139bde009f2dc
xav <xavier@loyaltylion.com>
2018-05-07 18:40:44 +0100

    can't have false timestamps

--- db/migrate/20180507000000_create_all_tables.rb
+++ db/migrate/20180507000000_create_all_tables.rb
@@ -9,7 +9,7 @@ class CreateAllTables < ActiveRecord::Migration[5.1]
       t.boolean :admin, default: false
       t.string :activation_digest
       t.boolean :activated, default: false
-      t.datetime :activated_at, default: false
+      t.datetime :activated_at
       t.string :reset_digest
       t.datetime :reset_sent_at
       t.string :initials
