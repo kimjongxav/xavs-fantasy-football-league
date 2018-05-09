217ba145cc3e2f169a22074ba215e658930a4a46
xav <xavier@loyaltylion.com>
2018-05-09 18:27:18 +0100

    show players in teams

--- app/controllers/premier_league_teams_controller.rb
+++ app/controllers/premier_league_teams_controller.rb
@@ -7,6 +7,20 @@ class PremierLeagueTeamsController < ApplicationController
 
   def show
     @premier_league_team = PremierLeagueTeam.find(params[:id])
+    unsorted_players = Player.where(:premier_league_team_id => @premier_league_team.id)
+    @players = sort_by_position(unsorted_players)
+  end
+
+  def sort_by_position(players)
+    positions = {
+      'GK' => 1,
+      'DEF' => 2,
+      'MID' => 3,
+      'FWD' => 4,
+    }
+    players.sort_by do |player|
+      positions[player.position]
+    end
   end
 
   def new
