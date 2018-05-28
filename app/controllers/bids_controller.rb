e1d9d94c4079af8106ccc52550e217152416ad30
xav <xavier@loyaltylion.com>
2018-05-28 19:41:35 +0100

    showinig bids better

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -21,10 +21,31 @@ class BidsController < TeamsController
       :window => 'summer',
       :round => 1,
     )
-    redirect_to :bids
+    redirect_to :team_bids
   end
 
   def available_formations
-    %w[3-4-3 3-5-2 4-5-1 4-4-2 4-3-3 5-4-1 5-3-2]
+    [
+      ['Select a formation', 0],
+      ['3-4-3', 1],
+      ['3-5-2', 2],
+      ['4-5-1', 3],
+      ['4-4-2', 4],
+      ['4-3-3', 5],
+      ['5-4-1', 6],
+      ['5-3-2', 7],
+    ]
+  end
+
+  def position_calculator(i, formation_id)
+    return 'GK' if i.zero?
+    formation = available_formations[formation_id].first
+    position_counts = formation.split('-')
+    defs = position_counts[0].to_i
+    mids = position_counts[1].to_i
+
+    return 'DEF' if i <= defs
+    return 'MID' if i <= (defs + mids)
+    'FWD'
   end
 end
