8ddbfe7a34f77e59479a8aae91a98d0f0f78d385
xav <xavier@loyaltylion.com>
2018-08-08 22:04:37 +0100

    scheduler stuff

--- app/services/calculate_team_points.rb
+++ app/services/calculate_team_points.rb
@@ -1,17 +1,5 @@
 class CalculateTeamPoints
   def self.call
-    url = 'https://fantasy.premierleague.com/drf/bootstrap-static'
-    response = HTTParty.get(url)
-    gameweek = JSON.parse(response.body)['current-event']
-
-    Team.all.each do |team|
-      players = team.players.where('gameweek_in <= 2 and gameweek_out is null')
-      captain = team.players.where(
-        'gameweek_in <= 3 and gameweek_out is null and captain_in <= 3 and captain_out is null',
-      ).first
-
-      current_gameweek_points = JSON.parse(team.gameweek_points)[gameweek - 1]
-
-    end
+    puts 'yes'
   end
 end
