6d0109757f8ee77d47f0d0e3428b7d82354aa4bc
xav <xavier@loyaltylion.com>
2018-08-15 12:42:25 +0100

    after gameweek finishes, show players for next gw

--- app/models/team.rb
+++ app/models/team.rb
@@ -34,7 +34,11 @@ class Team < ApplicationRecord
     url = 'https://fantasy.premierleague.com/drf/bootstrap-static'
     response = HTTParty.get(url)
     1 unless response
-    JSON.parse(response.body)['current-event'] if response.ok?
+    body = JSON.parse(response.body)
+    gameweek = body['current-event'] if response.ok?
+    finished = body['events'].select { |e| e['id'] == 1 }.first['finished']
+    return gameweek + 1 if finished
+    gameweek
   end
 
   def sort_by_position(players)
