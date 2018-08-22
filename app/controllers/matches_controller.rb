7e494a08b25b2c1b6917cf77c3fb647c8d315644
xav <xavier@loyaltylion.com>
2018-08-22 16:45:06 +0100

    match view

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,14 +1,36 @@
 class MatchesController < ApplicationController
-  before_action :logged_in_user, only: [:index]
+  before_action :logged_in_user, :only => [:index]
 
   def index
-    @matches = Match.paginate(page: params[:page])
+    @matches = Match.paginate(:page => params[:page])
   end
 
   def show
     @match = Match.find(params[:id])
+    @home_team = @match.home_team
+    @away_team = @match.away_team
+    @home_players = sort_by_position(home_players)
+    @away_players = sort_by_position(away_players)
+    @home_captain = home_captain
+    @away_captain = away_captain
   end
 
   def new
+  end
+
+  def home_players
+    @home_team.players.where('gameweek_in <= ? and (gameweek_out is null or gameweek_out > ?)', @match.gameweek, @match.gameweek)
+  end
+
+  def away_players
+    @away_team.players.where('gameweek_in <= ? and (gameweek_out is null or gameweek_out > ?)', @match.gameweek, @match.gameweek)
+  end
+
+  def home_captain
+    PlayerTeamRelationship.where(:team_id => @home_team.id, :captain => true).where('gameweek_in <= ? and (gameweek_out is null or gameweek_out > ?)', @match.gameweek, @match.gameweek).first.player
+  end
+
+  def away_captain
+    PlayerTeamRelationship.where(:team_id => @away_team.id, :captain => true).where('gameweek_in <= ? and (gameweek_out is null or gameweek_out > ?)', @match.gameweek, @match.gameweek).first.player
   end
 end
