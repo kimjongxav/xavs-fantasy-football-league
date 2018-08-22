46af53ccdcee70319f7de3e5142194a4e723ab51
xav <xavier@loyaltylion.com>
2018-08-22 14:38:29 +0100

    start showing all gameweeks in team view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,8 +9,20 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @players = @team.sort_by_position(@team.current_players)
     @bids = @team.bids
+    @gameweek = gameweek
   end
 
   def new
+  end
+
+  def gameweek
+    url = 'https://fantasy.premierleague.com/drf/bootstrap-static'
+    response = HTTParty.get(url)
+    1 unless response
+    body = JSON.parse(response.body)
+    gameweek = body['current-event'] if response.ok?
+    finished = body['events'].select { |e| e['id'] == 1 }.first['finished']
+    return gameweek + 1 if finished
+    gameweek
   end
 end
