adb0d98621b7f529124f4341522976989e13b3c7
xav <xavier@loyaltylion.com>
2018-08-28 13:23:23 +0100

    final fix?

--- app/services/calculate_match_scores.rb
+++ app/services/calculate_match_scores.rb
@@ -7,73 +7,73 @@ class CalculateMatchScores
     matches = Match.where(:gameweek => gameweek)
 
     matches.each do |match|
-    #   home_team_score = 0
-    #   away_team_score = 0
+      home_team_score = 0
+      away_team_score = 0
 
       home_team = match.home_team
       away_team = match.away_team
 
-    #   home_team_points = calculate_points(home_team, gameweek)
-    #   away_team_points = calculate_points(away_team, gameweek)
+      home_team_points = calculate_points(home_team, gameweek)
+      away_team_points = calculate_points(away_team, gameweek)
 
-    #   puts "home team: #{home_team.id} (#{home_team_points})"
-    #   puts "away team: #{away_team.id} (#{away_team_points})"
+      puts "home team: #{home_team.id} (#{home_team_points})"
+      puts "away team: #{away_team.id} (#{away_team_points})"
 
-    #   if home_team_points == away_team_points
-    #     home_team_score += 2
-    #     away_team_score += 2
+      if home_team_points == away_team_points
+        home_team_score += 2
+        away_team_score += 2
 
-    #     home_team.properties[:draws] += 1
-    #     home_team.properties_will_change!
-    #     home_team.save!
+        home_team.properties[:draws] += 1
+        home_team.properties_will_change!
+        home_team.save!
 
-    #     away_team.properties[:draws] += 1
-    #     away_team.properties_will_change!
-    #     away_team.save!
-    #   elsif home_team_points > away_team_points
-    #     home_team_score += 4
-    #     away_team_score += 1 if (home_team_points - away_team_points) <= 5
+        away_team.properties[:draws] += 1
+        away_team.properties_will_change!
+        away_team.save!
+      elsif home_team_points > away_team_points
+        home_team_score += 4
+        away_team_score += 1 if (home_team_points - away_team_points) <= 5
 
-    #     home_team.properties[:wins] += 1
-    #     home_team.properties_will_change!
-    #     home_team.save!
+        home_team.properties[:wins] += 1
+        home_team.properties_will_change!
+        home_team.save!
 
-    #     away_team.properties[:losses] += 1
-    #     away_team.properties[:matches_within_five_points] += 1 if (home_team_points - away_team_points) <= 5
-    #     away_team.properties_will_change!
-    #     away_team.save!
-    #   elsif home_team_points < away_team_points
-    #     away_team_score += 4
-    #     home_team_score += 1 if (away_team_points - home_team_points) <= 5
+        away_team.properties[:losses] += 1
+        away_team.properties[:matches_within_five_points] += 1 if (home_team_points - away_team_points) <= 5
+        away_team.properties_will_change!
+        away_team.save!
+      elsif home_team_points < away_team_points
+        away_team_score += 4
+        home_team_score += 1 if (away_team_points - home_team_points) <= 5
 
-    #     home_team.properties[:losses] += 1
-    #     away_team.properties[:matches_within_five_points] += 1 if (away_team_points - home_team_points) <= 5
-    #     home_team.properties_will_change!
-    #     home_team.save!
+        home_team.properties[:losses] += 1
+        away_team.properties[:matches_within_five_points] += 1 if (away_team_points - home_team_points) <= 5
+        home_team.properties_will_change!
+        home_team.save!
 
-    #     away_team.properties[:wins] += 1
-    #     away_team.properties_will_change!
-    #     away_team.save!
-    #   end
+        away_team.properties[:wins] += 1
+        away_team.properties_will_change!
+        away_team.save!
+      end
 
-    #   match.update!(
-    #     :home_score => home_team_score,
-    #     :away_score => away_team_score,
-    #     :home_points => home_team_points,
-    #     :away_points => away_team_points,
-    #     :played => true,
-    #   )
+      match.update!(
+        :home_score => home_team_score,
+        :away_score => away_team_score,
+        :home_points => home_team_points,
+        :away_points => away_team_points,
+        :played => true,
+      )
 
-    #   home_team.gameweek_scores[gameweek] = home_team_score
-    #   home_team.gameweek_scores_will_change!
-    #   home_team.save!
+      home_team.gameweek_scores[gameweek] = home_team_score
+      home_team.gameweek_scores_will_change!
+      home_team.save!
 
-    #   away_team.gameweek_scores[gameweek] = away_team_score
-    #   away_team.gameweek_scores_will_change!
-    #   away_team.save!
-      update_scores(gameweek, home_team, away_team)
+      away_team.gameweek_scores[gameweek] = away_team_score
+      away_team.gameweek_scores_will_change!
+      away_team.save!
     end
 
+    update_scores(gameweek)
   end
 
   def self.update_scores(gameweek, home_team, away_team)
@@ -89,6 +89,9 @@ class CalculateMatchScores
     over_sixty_teams = team_points.map { |k, v| k if v > 60 }.compact
 
     matches.each do |m|
+      home_team = m.home_team
+      away_team = m.away_team
+
       if most_points_teams.include?(m['home_team_id'])
         score = m['home_score']
         score += 1
