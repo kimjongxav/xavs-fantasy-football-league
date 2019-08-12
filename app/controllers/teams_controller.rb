c391fbc101863f5b45b9bd1d175b9bbf672c767e
kimjongxav <xavier@loyaltylion.com>
2019-08-12 19:45:38 +0100

    team gameweek changes again ffs

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,8 +9,14 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @players = @team.sort_by_position(@team.current_players)
     @bids = @team.bids
+    @gameweek = gameweek
   end
 
   def new
+  end
+
+  def gameweek
+    resp = HTTParty.get('https://fantasy.premierleague.com/api/bootstrap-static')
+    [resp['events'].find{|e| e['finished'] == false}['id'], 38].min  end
   end
 end
