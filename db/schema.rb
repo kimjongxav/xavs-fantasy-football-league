6e134b4504c5922076d99a561f701a0c5b2152ca
Nathaniel <nathanielmelman@gmail.com>
2019-08-20 23:53:57 +0100

    Added player pictures, formation and some css

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20190809200855) do
     t.string "full_name"
     t.string "common_name"
     t.string "position"
+    t.string "picture"
     t.text "gameweek_points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
