2327ba083ff7a33612b77f0b9c9f9a5dedabf0a7
Nathaniel <nathanielmelman@gmail.com>
2019-08-30 21:35:13 +0100

    Starting to pull apart the HTML and ruby functions

--- app/models/match.rb
+++ app/models/match.rb
@@ -11,3 +11,19 @@ class Match < ApplicationRecord
   scope :played, -> { where(:played => true) }
   scope :unplayed, -> { where(:played => false) }
 end
+
+def position(pos, team) 
+  team.select{|k| pos==k[1]}.map{|k| [k[0],k[2], k[3], k[4], k[5]]}
+end 
+
+def playerImage(i, pos) 
+  image_tag 'https://platform-static-files.s3.amazonaws.com/premierleague/photos/players/110x140/p' + pos[i][2] + '.png' 
+end 
+
+def teamScore()
+  players.map{ |pl| pl.gameweek_points[@match.gameweek] || 0}.sum + (captain.gameweek_points[@match.gameweek] || 0)
+end
+
+def linktoName()
+  link_to squad.name, squad
+end
